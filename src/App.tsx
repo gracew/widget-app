a26a639f6c7ebd920f350da8e4e430d0f7a4a468
Grace Wang <gracewang92@gmail.com>
2020-02-20 00:53:53 -0800

    add apollo-client deps and actually talk to BE

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
@@ -8,20 +10,26 @@ import { NameAPI } from "./components/NameAPI";
 import { Navbar } from "./components/Navbar";
 import { TryAPI } from "./components/TryAPI";
 
+const client = new ApolloClient({
+  uri: "http://localhost:8080/query"
+});
+
 function App() {
   return (
-    <Router>
-      <Route path="/" component={Navbar} />
-      <div className="App">
-        <Switch>
-          <Route path="/new" exact={true} component={NameAPI} />
-          <Route path="/define" exact={true} component={DefineAPI} />
-          <Route path="/auth" exact={true} component={AuthAPI} />
-          <Route path="/try" exact={true} component={TryAPI} />
-          <Route path="/all" exact={true} component={ListAPIs} />
-        </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route path="/" component={Navbar} />
+        <div className="App">
+          <Switch>
+            <Route path="/new" exact={true} component={NameAPI} />
+            <Route path="/define" exact={true} component={DefineAPI} />
+            <Route path="/auth" exact={true} component={AuthAPI} />
+            <Route path="/try" exact={true} component={TryAPI} />
+            <Route path="/all" exact={true} component={ListAPIs} />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/components/ListAPIs.tsx
+++ src/components/ListAPIs.tsx
@@ -1,31 +1,48 @@
+import { useQuery } from "@apollo/react-hooks";
 import { Icon } from "@blueprintjs/core";
+import { gql } from "apollo-boost";
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
 
-export class ListAPIs extends React.Component<RouteComponentProps> {
-  public render() {
-    return (
-      <table className="bp3-html-table">
-        <thead>
-          <tr>
-            <th>API</th>
-            <th>Sandbox</th>
-            <th>Production</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Posts</td>
-            <td><Icon icon="tick-circle" intent="success" /></td>
-            <td><Icon icon="tick-circle" intent="primary" /></td>
-          </tr>
-          <tr>
-            <td>Comments</td>
-            <td><Icon icon="tick-circle" intent="success" /></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    );
+const ALL_APIS = gql`
+  {
+    apis {
+      id
+      name
+    }
   }
+`;
+
+export function ListAPIs() {
+  const { loading, error, data } = useQuery(ALL_APIS);
+  // TODO(gracew): handle loading/error states better
+  if (loading) {
+    return <p>Loading</p>;
+  }
+  if (error) {
+    return <p>Error</p>;
+  }
+  return (
+    <table className="bp3-html-table">
+      <thead>
+        <tr>
+          <th>API</th>
+          <th>Sandbox</th>
+          <th>Production</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.apis.map(({ name }: { name: string }) => (
+          <tr>
+            <td>{name}</td>
+            <td>
+              <Icon icon="tick-circle" intent="success" />
+            </td>
+            <td>
+              <Icon icon="tick-circle" intent="primary" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }

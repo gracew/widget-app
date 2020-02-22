77fe21816bb77116608c587d6267fcdc1fc81666
Grace Wang <gracewang92@gmail.com>
2020-02-21 20:20:07 -0800

    plumb defineAPI mutation

--- src/components/DefineAPI.tsx
+++ src/components/DefineAPI.tsx
@@ -1,14 +1,44 @@
+import { useMutation } from "@apollo/react-hooks";
 import { Button, FileInput } from "@blueprintjs/core";
+import { gql } from "apollo-boost";
 import React from "react";
 import MonacoEditor from "react-monaco-editor";
 import { useHistory, useParams } from "react-router-dom";
 import { DEPLOY } from "../routes";
 import "./DefineAPI.css";
 
+const DEFINE_API = gql`
+  mutation DefineAPI($apiID: ID!, $rawDefinition: String!) {
+    defineAPI(input: { apiID: $apiID, rawDefinition: $rawDefinition }) {
+      fields {
+        name
+        type
+      }
+    }
+  }
+`;
+
+const EXAMPLE = `
+[
+  {
+    "name": "Game",
+    "fields": [{ "name": "name", "type": "String!" }],
+    "operations": []
+  }
+]
+`;
+
 export function DefineAPI() {
   const history = useHistory();
+  const [defineApi, _] = useMutation(DEFINE_API);
   const { id } = useParams();
-  const handleNext = () => history.push(`/${id}${DEPLOY}`);
+  let text = EXAMPLE;
+
+  async function handleNext() {
+    await defineApi({ variables: { apiID: id, rawDefinition: text } });
+    history.push(`/${id}${DEPLOY}`);
+  }
+
   return (
     <div>
       <h2>Define your API</h2>
@@ -16,7 +46,10 @@ export function DefineAPI() {
         width="800"
         height="600"
         theme="vs-dark"
-        value="hello world!"
+        value={EXAMPLE}
+        onChange={newValue => {
+          text = newValue;
+        }}
       />
       <div className="upload-api">
         <p>Upload a file instead</p>

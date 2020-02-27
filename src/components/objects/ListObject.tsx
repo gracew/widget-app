2685c55062a409c35a35e072eac2fbc29ec8fdde
Grace Wang <gracewang92@gmail.com>
2020-02-27 01:17:55 -0800

    page size form

--- src/components/objects/ListObject.tsx
+++ src/components/objects/ListObject.tsx
@@ -1,4 +1,10 @@
-import { Button, HTMLSelect, InputGroup } from "@blueprintjs/core";
+import {
+  Button,
+  FormGroup,
+  HTMLSelect,
+  InputGroup,
+  NumericInput
+} from "@blueprintjs/core";
 import React, { useState } from "react";
 import { FormAndResult } from "./FormAndResult";
 
@@ -10,11 +16,14 @@ export function ListObject({ definition }: IListObjectProps) {
   const includeList = definition.operations.find(
     (el: any) => el.type === "LIST"
   );
+  const [pageSize, setPageSize] = useState();
   const [output, setOutput] = useState("");
-  const onClick = () =>
-    fetch(`http://localhost:8080/apis/${definition.name}/STAGING`)
+  const onClick = () => {
+    const query = pageSize ? `?pageSize=${pageSize}` : "";
+    fetch(`http://localhost:8080/apis/${definition.name}/STAGING${query}`)
       .then(res => res.text())
       .then(t => setOutput(t));
+  };
   return (
     <FormAndResult output={output}>
       {includeList && includeList.sort && includeList.sort.length > 0 && (
@@ -42,6 +51,9 @@ export function ListObject({ definition }: IListObjectProps) {
           <InputGroup />
         </div>
       )}
+      <FormGroup label="Page Size (default 100)">
+        <NumericInput onValueChange={(num: number) => setPageSize(num)} />
+      </FormGroup>
       <Button icon="play" text="Run" intent="primary" onClick={onClick} />
       <Button icon="duplicate" text="Copy cURL" />
     </FormAndResult>
